44f72e9f972e6e7fd1ee3e60190f7dfc0d149f0a
Shreya M Parsekar <shreyaparsekar22@gmail.com>
2023-06-21 19:34:11 +0530

    ~

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,81 @@
+import './index.scss'
+import LogoS from '../../assets/images/favicon.png'
+import BehanceLogo from '../../assets/images/Behance.png'
+import LogoSubtitle from '../../assets/images/Add.PNG'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+  faScrewdriverWrench,
+} from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+  return (
+    <>
+      <div className="nav-bar">
+        <Link className="logo" to="/">
+          <img src={LogoS} alt="Logo" />
+          <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+        </Link>
+        <nav>
+          <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
+          <NavLink activeclassname="active" className="about-link" to="/about">
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
+
+          <NavLink
+            activeclassname="active"
+            className="skills-link"
+            to="/skills"
+          >
+            <FontAwesomeIcon icon={faScrewdriverWrench} color="#4d4d4e" />
+          </NavLink>
+
+          <NavLink
+            activeclassname="active"
+            className="contact-link"
+            to="/contact"
+          >
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </NavLink>
+        </nav>
+        <ul>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/shreya-maniksha-parsekar-7241a81b2/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} color="#b9b9b9" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/Shrpar22"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} color="#b9b9b9" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.behance.net/shreyaparsekar"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={BehanceLogo} width="24" alt="Behance" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Sidebar
